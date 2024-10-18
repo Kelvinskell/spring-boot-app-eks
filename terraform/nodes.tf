c12d0ed980a380b867d7c699952bf7881f729265
Kelvinskell <kelvinskelll@gmail.com>
2024-10-18 02:15:45 +0100

    Update roles dependency

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -65,7 +65,7 @@ resource "aws_eks_node_group" "general" {
   depends_on = [
     aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
     aws_iam_role_policy_attachment.amazon_eks_cni_policy,
-    aws_iam_role_policy_attachment.amazon_ec2_container_registry_read_only,
+    aws_iam_role_policy_attachment.amazon_eks_csi_policy,
   ]
 
   # Allow external changes without Terraform plan difference
